1d05354082e5d56bf50f4e8694ad772d1b27a7fb
choiwonwong <chldnjsdnd12@gmail.com>
2025-09-10 00:04:20 +0900

    spark option for standalone

--- join_test/test_utils.py
+++ join_test/test_utils.py
@@ -95,11 +95,12 @@ class TimeTracker:
 def create_spark_session(app_name: str, aqe_enable: bool = True) -> SparkSession:
     builder = SparkSession.builder \
         .appName(app_name) \
-        .config("spark.driver.memory", "2g") \
-        .config("spark.driver.cores", "2") \
-        .config("spark.executor.memory", "2g") \
-        .config("spark.executor.instances", "2") \
-        .config("spark.executor.cores", "1")
+        .master("local[2]") \
+        .config("spark.driver.memory", "4g")
+        # .config("spark.driver.cores", 2) \ # Can't set in standalone mode
+        # .config("spark.executor.memory", "2g") \
+        # .config("spark.executor.instances", 2) \
+        # .config("spark.executor.cores", 1)
 
     if aqe_enable:
         builder = builder \
